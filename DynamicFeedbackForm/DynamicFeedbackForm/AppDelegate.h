8058d44ae1f954794c8cd9041032f71f93d7cf0a
Jan Hennings <hennings.jan@me.com>
2014-05-06 15:11:18 +0200

    Initial commit

---
+++ DynamicFeedbackForm/DynamicFeedbackForm/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DynamicFeedbackForm
+//
+//  Created by Jan Hennings on 5/6/14.
+//  Copyright (c) 2014 Jan Hennings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DynamicFeedbackForm/DynamicFeedbackForm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DynamicFeedbackForm
+//
+//  Created by Jan Hennings on 5/6/14.
+//  Copyright (c) 2014 Jan Hennings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
